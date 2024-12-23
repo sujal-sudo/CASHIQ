5be8ff15b2bd5cc4dcfb059d4a7082570d20a66d
sujal-sudo <np.sujal2004@gmail.com>
2024-12-23 15:49:05 +0545

    created splashscreen, loginActivity, SignupActivity

--- app/src/main/java/com/example/cashiq/OnboardingActivity.kt
+++ app/src/main/java/com/example/cashiq/OnboardingActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Register : AppCompatActivity() {
+class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_onboarding)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
