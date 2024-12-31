dc3d7b20112c292d59f837aeac57bb555f3ca748
nipuana <nipuanarimal@gmail.com>
2024-12-31 13:36:27 +0545

    added logo for the app screen and managed packages

--- app/src/main/java/com/example/cashiq/UI/activity/LoginActivity.kt
+++ app/src/main/java/com/example/cashiq/UI/activity/LoginActivity.kt
@@ -1,15 +1,11 @@
-package com.example.cashiq
+package com.example.cashiq.UI.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import com.example.cashiq.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
