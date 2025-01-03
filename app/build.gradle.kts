2c63608af961a0b40063825a0aa1e564098acf99
Aasu703 <smellylips508@gmail.com>
2025-01-03 19:33:41 +0545

    Added Database to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
+    buildFeatures {
+        viewBinding = true
+    }
     namespace = "com.example.cashiq"
     compileSdk = 35
 
@@ -16,6 +20,10 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
+
+
+
 
     buildTypes {
         release {
@@ -43,6 +51,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.play.services.gcm)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/cashiq/UserData.kt
@@ -0,0 +1,9 @@
+package com.example.cashiq
+
+data class UserData(
+    // storing 3 data in realtime database
+    val id: String? = null,
+    val username: String? = null,
+    val email: String? =null,
+    val password: String? = null,
+)
