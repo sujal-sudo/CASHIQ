fa730b873978c92993fb4be3310765965e230ff9
Aasu703 <smellylips508@gmail.com>
2025-01-23 13:54:57 +0545

    changes done

--- app/src/main/java/com/example/cashiq/model/Transaction.kt
+++ app/src/main/java/com/example/cashiq/model/Transaction.kt
@@ -1,6 +1,12 @@
 package com.example.cashiq.model
+import com.example.cashiq.model.UserData
+import com.google.firebase.database.DatabaseReference
+
+private lateinit var databaseReference: DatabaseReference
 
 data class Transaction(
+    val transactionId : String = "",
+    val userId: String? = null,
     val amount: Double,
     val category: String,
     val date: String,
