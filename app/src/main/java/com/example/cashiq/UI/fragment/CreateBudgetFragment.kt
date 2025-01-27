fd054f1f911d78aaeb2553471e8e8f70cf8a077e
Aasu703 <smellylips508@gmail.com>
2025-01-27 22:15:11 +0545

    commit for merge

--- app/src/main/java/com/example/cashiq/UI/fragment/CreateBudgetFragment.kt
+++ app/src/main/java/com/example/cashiq/UI/fragment/CreateBudgetFragment.kt
@@ -35,7 +35,7 @@ class CreateBudgetFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_budget2, container, false)
+        return inflater.inflate(R.layout.fragment_create_budget, container, false)
     }
 
     companion object {
--- app/src/main/java/com/example/cashiq/model/BudgetViewModel.kt
+++ app/src/main/java/com/example/cashiq/model/BudgetViewModel.kt
@@ -1,4 +1,10 @@
 package com.example.cashiq.model
 
-class BudgetViewModel {
-}
+data class Budget(
+    val budgetId: String = "",
+    val userId: String = "",
+    val category: String = "",
+    val amount: Double = 0.0,
+    val startDate: String = "", // Use ISO format
+    val endDate: String = ""   // Use ISO format
+)
